bdc8a96c66bdaf53fa02b2a0522d3bfc485bff3b
Here2ServeU <revnaweji@gmail.com>
2026-01-13 21:49:17 -0600

    Added the app directory

--- app/healthcheck.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-
-const req = http.request(
-  { host: "localhost", port: 3000, path: "/health", method: "GET", timeout: 2000 },
-  (res) => process.exit(res.statusCode === 200 ? 0 : 1)
-);
-req.on("error", () => process.exit(1));
-req.end();
